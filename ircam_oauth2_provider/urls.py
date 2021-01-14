746bbd7ff6fbe041ef7a3ea728673f83f953729d
drrr-deudeu <denis.rouiller@gmail.com>
2021-01-14 13:29:01 +0100

    Mezzo compatibility and normally staying compatible with forum

--- ircam_oauth2_provider/urls.py
+++ ircam_oauth2_provider/urls.py
@@ -1,5 +1,9 @@
-from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
-
+from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns,url
 from .provider import IrcamAuthProvider
+from django.conf import settings
 
-urlpatterns = default_urlpatterns(IrcamAuthProvider)
+try:
+    if settings.OAUTH2_IRCAM == True:
+        urlpatterns = default_urlpatterns(IrcamAuthProvider)
+except AttributeError:
+    urlpatterns = default_urlpatterns(IrcamAuthProvider)
--- ircam_oauth2_provider/views.py
+++ ircam_oauth2_provider/views.py
@@ -3,11 +3,20 @@ from allauth.socialaccount.providers.oauth2.views import (OAuth2Adapter, OAuth2L
 from .provider import IrcamAuthProvider
 from django.conf import settings
 
+try:
+    if settings.OAUTH2_IRCAM is not None:
+        logger = logging.getLogger('ircamauth')
+        logger.addHandler(logging.StreamHandler())
+        logger.setLevel(logging.DEBUG) if settings.DEBUG else logger.setLevel(logging.WARNING)
+except AttributeError:
+    pass
 
 class IrcamAuthAdapter(OAuth2Adapter):
 
     if not hasattr(settings, 'OAUTH_SERVER_BASEURL'):
         raise Exception("Couldn't find OAUTH_SERVER_BASEURL in the settings")
+
+    logger.setLevel(logging.DEBUG) if settings.DEBUG else logger.setLevel(logging.WARNING)
 
     provider_id = IrcamAuthProvider.id
     access_token_url = '{}/o/token/'.format(settings.OAUTH_SERVER_BASEURL)  # Called programmatically, must be reachable from container
@@ -18,7 +27,17 @@ class IrcamAuthAdapter(OAuth2Adapter):
         headers = {'Authorization': 'Bearer {0}'.format(token.token)}
         resp = requests.get(self.profile_url, headers=headers)
         extra_data = resp.json()
-        return self.get_provider().sociallogin_from_response(request, extra_data)
+        try:
+            if settings.OAUTH2_IRCAM is not None:
+                if createOrUpdateLocalEntities(extra_data):
+                    social_login = self.get_provider().sociallogin_from_response(request, extra_data)
+                else :
+                    logger.error("Failed to create user and socialAccount with extra_data: {0}".format(extra_data))
+                    return None
+                return social_login
+        # allows forum to make is own workflow
+        except AttributeError:
+            return self.get_provider().sociallogin_from_response(request, extra_data)
 
 
 oauth2_login = OAuth2LoginView.adapter_view(IrcamAuthAdapter)
